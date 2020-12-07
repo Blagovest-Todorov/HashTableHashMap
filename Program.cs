c4d37356807c61c94f1728da3040051bc3ade0dc
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-12-07 10:11:49 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _13.HashTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            //Hash Table - > Hash Map !!!! is adata structure ! that implemetns associative array 
+            // a hash talbe uses hash Functon to compute an index
+
+
+            //data structure -> (format of)  data organisation ,management,
+            //storage  type format ,  that enables  access and mofification.
+            //Data structure is : collection of data values, the relations among them, 
+            // the functions or  operations that can be applied on that data !
+            // hash talbe uses hash function to copmpute Index , key ; from which the desired value can be found.
+            // hash table is more effcient ! A function shoud provide uniform distribution of hash valies to avoid collisions
+
+            // The main advantage of the hash table over other table data structures is the speed !
+            // Hash talbles are powerful when they contain a large numbers of entities.
+            // for a small number of entiteies are not effective
+            //a dictionary is implementation of hash Table
+            // Hash Map = Hash Table
+        }
+    }
+}
